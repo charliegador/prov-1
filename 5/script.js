d07db48e70542b806daafc7e58eb8f32c5ea44f2
Charlie Gador <charlie.gador@skola.botkyrka.se>
2020-10-19 13:56:20 +0200

    Uppgift 5, väntar

---
+++ 5/script.js
@@ -0,0 +1,6 @@
+//fråga 5, Skapa en funktion areaofTriangle som accepterar lämpliga argument och returnerar arean av en triangel
+function areaOfTriangle(base, height){
+    let area = base * height;
+}
+let result = areaOfTriangle(base = 5, height = 5)
+console.log(result)
