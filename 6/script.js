4ee167514a395b40b88356f6bca6f8dcfa34bbb7
Charlie Gador <charlie.gador@skola.botkyrka.se>
2020-10-19 14:06:53 +0200

    Uppgift 7, väntar

---
+++ 6/script.js
@@ -0,0 +1,6 @@
+//skapa en funktion isSameUser som accepterar två objekt som argument. Funktionen returnerar true ifall objektens property email är identiska, annars false
+function isSameUser({objkt1, objkt2}){
+    if (fds){
+        
+    }
+}
---
+++ 7/script.js
@@ -0,0 +1,4 @@
+//skriv en funktion makeCity som accepterar ett namn som argument och returnerar  ett objekt med följande properties
+function makeCity(){
+    
+}
