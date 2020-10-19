cf36e6a47747f98f4ddad8d62d4ab99f085284d7
Charlie Gador <charlie.gador@skola.botkyrka.se>
2020-10-19 13:46:45 +0200

    Uppgift 3 (väntar med 2)

---
+++ 2/script.js
@@ -0,0 +1,6 @@
+//Fråga 2
+function sum(tal1, tal2, tal3){
+    tal1 + tal2 + tal3
+}
+let result = sum(1, 2, 4)
+console.log(result)
---
+++ 3/script.js
@@ -0,0 +1,6 @@
+//Skapa en array med 5 strängar, Byt sedan ut första och sista elementet mot valfria tal
+let fiveStrings = [1, 2, 3, 4, 5];
+
+fiveStrings = [10, 2, 3, 4, 10];
+
+console.log(fiveStrings);
