3c456fed173af299d9314bb315d1d6500949f2ff
Charlie Gador <charlie.gador@skola.botkyrka.se>
2020-10-19 13:51:45 +0200

    Väntar med uppgift 4

---
+++ 4/script.js
@@ -0,0 +1,6 @@
+//skriv en funktion lastElement som accepterar en array som argument och returnerar det sista elementet
+function lastElement(array){
+    
+}
+let result = lastElement([1, 2, 3])
+console.log()
