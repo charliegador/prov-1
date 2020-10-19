afb34122b49419f7ed11da86cf34dfccfcfa94a4
Charlie Gador <charlie.gador@skola.botkyrka.se>
2020-10-19 14:25:26 +0200

    uppgift 10, igen

--- 10/script.js
+++ 10/script.js
@@ -2,7 +2,7 @@ function orkarInteMer(array){
  if (array%3 === 0){
      return [9,3];
  } else {
-     return false
+     return //Vet att det inte ska vara if, else, men har inget annat..
  }
 }
 let result = orkarInteMer(4,7,9,3,8);
