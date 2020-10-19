38b9346e37c81a716f06f832c55b991184ac9fee
Charlie Gador <charlie.gador@skola.botkyrka.se>
2020-10-19 14:12:16 +0200

    Uppgift 9, väntar

---
+++ 8/script.js
@@ -0,0 +1,5 @@
+//skriv en funktion makeRandomArray som inte tar några argument, den returnerar en array med fyra olika random tal mellan 0 och 1
+//Eller gör random tal ba
+function makeRandomArray(){
+
+}
